0137acf25d1555a9f9bbfaba56abbf1d15082dbd
VISHNU PRASAD N <user123@localhost.localdomain>
2023-10-23 10:29:15 +0530

    get methord added

--- server/main.js
+++ server/main.js
@@ -8,10 +8,13 @@ const mailRoute = require('./routes/mail.route')
 
 // middlewares
 app.use(cors())
-app.use(express.urlencoded({extended: true})); 
-app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use('/', mailRoute)
+app.get('/', (req, res) => {
+    res.send("Welcome to Green House")
+})
 
 app.listen(serverConfig.PORT, () => {
     console.log('server is running port', serverConfig.PORT);
